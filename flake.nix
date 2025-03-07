88102876c744873ab89a1b25f5b7aeb7abbfa7a6
Gautham <gauthsvenkat@gmail.com>
2025-03-07 15:52:07 +0100

    thinkpad as well

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     in
     {
       nixosConfigurations = {
-        # thinkpad = mkNixosSystem "thinkpad" "x86_64-linux" "ando";
+        thinkpad = mkNixosSystem "thinkpad" "x86_64-linux" "ando";
         xps = mkNixosSystem "xps" "x86_64-linux" "gautham";
         elitedesk = mkNixosSystem "elitedesk" "x86_64-linux" "ando";
       };
