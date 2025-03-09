6269f8d17402681baa1e569a6e259d4aaa862c6a
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-03-09 14:41:13 +0100

    Squash merge elitedesk-setup-zpool into main

--- hosts/elitedesk/default.nix
+++ hosts/elitedesk/default.nix
@@ -14,4 +14,16 @@
   home-manager.users.${username}.programs.git = {
     userEmail = "gauthsvenkat@gmail.com";
   };
+
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    zfs = {
+      # By default, following option is true for legacy reasons.
+      forceImportRoot = false;
+      extraPools = [ "dontdie" ];
+    };
+  };
+
+  # ZFS requires networking.hostId to be set
+  networking.hostId = "0c0ffee0";
 }
