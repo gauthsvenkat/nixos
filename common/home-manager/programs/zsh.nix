426211c437a8698423f69b2491f973664ef0dc9a
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-03-26 15:28:38 +0100

    fastfetch is back bby

--- common/home-manager/programs/zsh.nix
+++ common/home-manager/programs/zsh.nix
@@ -25,6 +25,7 @@
     initExtra = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       test -f ~/.p10k.zsh && source ~/.p10k.zsh
+      ${pkgs.fastfetch}/bin/fastfetch
     '';
   };
 }
