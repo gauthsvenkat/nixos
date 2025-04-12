6ec920c948e4cf32788ae906645e0ddc253b99c8
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-04-12 16:40:40 +0200

    Squash merge upgrade into main

--- common/public_keys.nix
+++ common/public_keys.nix
@@ -1,7 +1,7 @@
 {
   ando = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7C+VPx3ZLurAON5pknD1AlkvdsmB+0J63Yo8y7MVMJ ando@thinkpad"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjZq6GCEU+TpzLRthwvjzN6pPO+gJt2ngakYpxycf+y ando@thunderdome"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINyRTUCgazY6gGUf76EMhgroZ1fFPFMHcX0UkpkPJkDk ando@thunderdome"
   ];
 
   nixpi = [
