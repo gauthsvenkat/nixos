ec26e2b6c1de7758f3b08e33048a8b96fe97ee82
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-04-26 18:18:26 +0200

    gaming is a bust

--- hosts/thunderdome/default.nix
+++ hosts/thunderdome/default.nix
@@ -22,6 +22,10 @@ in
     ../../common/services/zfs.nix
   ];
 
+  # If enabled, the boot process get randomly stuck on "A start job
+  # is running for /dev/tpmrm0".
+  systemd.tpm2.enable = false;
+
   boot.zfs.extraPools = [ zpool ];
   services.sanoid.datasets."${zpool}".use_template = [ "default" ];
 
