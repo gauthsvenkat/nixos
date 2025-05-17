961847261f0a2244540ca28b523f451dc55586ff
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-05-17 11:33:30 +0200

    basic hyprland config

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
+{ username, pkgs, ... }:
 {
   programs.hyprland.enable = true;
 
   environment.systemPackages = [ pkgs.kitty ];
+
+  home-manager.users.${username}.wayland.windowManager.hyprland.settings =
+    let
+      terminal = "wezterm";
+      mainMod = "SUPER";
+    in
+    {
+      bind = [
+        "${mainMod}, Enter, exec, ${terminal}"
+      ];
+    };
 }
