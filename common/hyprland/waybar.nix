db7e7a7d9ed5ea5f2ed7263ce0fb575f9329845f
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-05-22 07:15:20 +0200

    WIP

--- common/hyprland/waybar.nix
+++ common/hyprland/waybar.nix
@@ -1,7 +1,106 @@
-#TODO: Useful widgets and spice it up
+#TODO= Useful widgets and spice it up
 { username, ... }:
 {
   home-manager.users.${username}.programs.waybar = {
     enable = true;
+    settings = {
+      main_bar = {
+        layer = "top";
+        position = "bottom";
+        spacing = 4;
+
+        "custom/appmenu" = {
+          format = "Apps";
+          on-click = "rofi -show drun";
+        };
+
+        "hyprland/window" = {
+          format = "{initialTitle}";
+        };
+
+        "hyprland/workspaces" = {
+          format = "{icon}";
+          format-icons = {
+            active = "";
+            default = "";
+          };
+        };
+
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
+        };
+        memory = {
+          format = "{}% ";
+        };
+
+        clock = {
+          interval = 1;
+          format = "{=%R=%S}  ";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "month";
+            weeks-pos = "left";
+            format = {
+              months = "<span color='#ffffff'><b>{}</b></span>";
+              days = "<span color='#ffffff'><b>{}</b></span>";
+              weeks = "<span color='#ffffff'><b>W{}</b></span>";
+              weekdays = "<span color='#ffffff'><b>{}</b></span>";
+              today = "<span color='#ffffff'><b><u>{}</u></b></span>";
+            };
+          };
+        };
+
+        modules-left = [
+          "custom/appmenu"
+          "hyprland/window"
+        ];
+
+        modules-center = [
+          "hyprland/workspaces"
+        ];
+
+        modules-right = [
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "clock"
+        ];
+      };
+    };
+
+    style = ''
+      * {
+          border-radius: 8px;
+          font-family: JetBrainsMono;
+          font-size: 16px;
+      }
+
+      window#waybar {
+          background-color: transparent;
+      }
+
+      button {
+          border: none;
+      }
+      button:hover {
+          box-shadow: inset 0 -3px #ffffff;
+      }
+
+      tooltip {
+          background-color: rgba(0,0,0,0.8);
+      }
+
+      #custom-appmenu,
+      #window,
+      #cpu,
+      #memory,
+      #clock {
+          padding: 0 8px;
+          border: 1px solid #ffffff;
+          background-color: rgba(0,0,0,0.8);
+      }
+    '';
   };
 }
