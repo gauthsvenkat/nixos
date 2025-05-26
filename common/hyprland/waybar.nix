4d35851ac873f3ff0ae3ecb853f12e1b3c35d334
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-05-26 20:20:03 +0200

    waybar config

--- common/hyprland/waybar.nix
+++ common/hyprland/waybar.nix
@@ -9,92 +9,68 @@
         position = "bottom";
         spacing = 4;
 
-        "custom/appmenu" = {
-          format = "Apps";
-          on-click = "tofi-drun --drun-launch=true";
-        };
-
-        "hyprland/workspaces" = {
-          format = "{icon}";
-          format-icons = {
-            active = "";
-            default = "";
-          };
-        };
-
-        cpu = {
-          format = "{usage}% ";
-          tooltip = false;
-        };
-        memory = {
-          format = "{}% ";
-        };
-
-        clock = {
-          interval = 1;
-          format = "{=%R=%S}  ";
-          tooltip-format = "<tt><small>{calendar}</small></tt>";
-          calendar = {
-            mode = "month";
-            weeks-pos = "left";
-            format = {
-              months = "<span color='#ffffff'><b>{}</b></span>";
-              days = "<span color='#ffffff'><b>{}</b></span>";
-              weeks = "<span color='#ffffff'><b>W{}</b></span>";
-              weekdays = "<span color='#ffffff'><b>{}</b></span>";
-              today = "<span color='#ffffff'><b><u>{}</u></b></span>";
-            };
-          };
-        };
-
         modules-left = [
-          "custom/appmenu"
-          "hyprland/window"
         ];
 
         modules-center = [
-          "hyprland/workspaces"
         ];
 
         modules-right = [
           "cpu"
-          "memory"
+
+          "bluetooth"
+          "backlight"
+          "battery"
+
           "clock"
         ];
+
+        cpu.format = " {usage}%";
+
+        bluetooth = {
+          format-off = "󰂲";
+          format-on = "";
+          format-connected = "󰂱";
+          tooltip-format = "{status} \n {num_connections} connected \n {device_alias}";
+        };
+
+        #TODO: Stuff like backlight and battery is only relevant for laptops. Enable it conditionally
+        backlight = {
+          format = "{icon} {percent}%";
+          format-icons = [
+            "󰃞"
+            "󰃠"
+          ];
+          reverse-scrolling = true;
+          scroll-step = 2.0;
+          tooltip = false;
+        };
+
+        battery = {
+          interval = 15;
+          states = {
+            warning = 40;
+            critical = 20;
+          };
+          format = "{icon} {capacity}%";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          tooltip-format = "{power}W | {time}";
+        };
+
+        clock = {
+          interval = 1;
+          format = " :%H:%M:%S";
+          # TODO: Specific this in some global constants?
+          timezone = "Europe/Amsterdam";
+          tooltip-format = "%D";
+        };
       };
     };
-
-    style = ''
-      * {
-          border-radius: 8px;
-          font-family: JetBrainsMono;
-          font-size: 16px;
-      }
-
-      window#waybar {
-          background-color: transparent;
-      }
-
-      button {
-          border: none;
-      }
-      button:hover {
-          box-shadow: inset 0 -3px #ffffff;
-      }
-
-      tooltip {
-          background-color: rgba(0,0,0,0.8);
-      }
-
-      #custom-appmenu,
-      #window,
-      #cpu,
-      #memory,
-      #clock {
-          padding: 0 8px;
-          border: 1px solid #ffffff;
-          background-color: rgba(0,0,0,0.8);
-      }
-    '';
   };
 }
