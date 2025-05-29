01412b33f41a0b846151ab2162ea4b0a4d58305b
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-05-29 20:37:24 +0200

    remove authorized keys from ssh

--- common/services/ssh.nix
+++ common/services/ssh.nix
@@ -1,14 +1,3 @@
-{ username, ... }:
 {
   services.openssh.enable = true;
-
-  users.users =
-    let
-      _all_keys = (import ../../public_keys.nix);
-      keys = _all_keys.ando;
-    in
-    {
-      ${username}.openssh.authorizedKeys.keys = keys;
-      root.openssh.authorizedKeys.keys = keys;
-    };
 }
