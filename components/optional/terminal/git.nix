04f53df240c5879c7c6d3dc061f9f781a3514db1
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-05-30 15:24:27 +0200

    update git settings

--- components/optional/terminal/git.nix
+++ components/optional/terminal/git.nix
@@ -3,9 +3,19 @@
   home-manager.users.${username}.programs = {
     git = {
       enable = true;
+
+      difftastic = {
+        enable = true;
+        enableAsDifftool = true;
+      };
+
       lfs.enable = true;
+
       userName = "Gautham Venkataraman";
       userEmail = "gauthsvenkat@gmail.com";
+
+      extraConfig.init.DefaultBranch = "main";
+
       ignores = [
         ".envrc"
         ".env"
