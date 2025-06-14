7279841ffe9c32a2557f9192f0c849b84820720a
Gautham <gauthsvenkat@gmail.com>
2025-06-14 14:46:31 +0200

    Disable spotify autostart

--- components/extra/spotify.nix
+++ components/extra/spotify.nix
@@ -6,8 +6,9 @@
   home-manager.users.${username} = {
     home.packages = [pkgs.spotify];
 
-    xdg.autostart.entries = [
-      "${pkgs.spotify}/share/applications/spotify.desktop"
-    ];
+    #NOTE: disabling spotify autostart in favor of spotify-player
+    # xdg.autostart.entries = [
+    #   "${pkgs.spotify}/share/applications/spotify.desktop"
+    # ];
   };
 }
